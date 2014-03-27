d2dca1faa29e5d1d9df19ea3fbafac4259c053ef
Liam Kilroy <Liam@Danas-MacBook-Pro.local>
2014-03-26 21:13:36 -0700

    Added more code regarding initial UI. Several small bugs. Will patch soon.

--- Artemis/AppDelegate.m
+++ Artemis/AppDelegate.m
@@ -25,7 +25,29 @@
         UIViewController *viewController = [[UIViewController alloc] init];
         UIView *view = [[UIView alloc] initWithFrame:viewRect];
         
-        view.backgroundColor = [UIColor lightGrayColor];
+        view.backgroundColor = [UIColor lightGreenColor];
+        
+        viewController.view = view;
+        
+        self.window.rootViewController = viewController;
+        [self.window makeKeyAndVisible];
+        
+        NSLog(@"Screen is %f tall and %f wide", viewRect.size.height, viewRect.size.width);
+        
+        return YES;
+    }
+    @end
+#import "AppDelegate.h"
+    
+    @implementation AppDelegate
+    - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+    {
+        CGRect viewRect = [[UIScreen mainScreen] bounds];
+        self.window = [[UIWindow alloc] initWithFrame:viewRect];
+        UIViewController *viewController = [[UIViewController alloc] init];
+        UIView *view = [[UIView alloc] initWithFrame:viewRect];
+        
+        view.backgroundColor = [UIColor lightGreenColor];
         
         viewController.view = view;
         
@@ -69,4 +91,5 @@
     
     
     
-
+@end
+@end
