cd9faecb669d0324aebeffd688839ada60e1c231
Colin Loretz <colin@cloudsnap.com>
2014-04-02 19:02:30 -0700

    fixed artemistextbox hanging around

--- Artemis/AppDelegate.m
+++ Artemis/AppDelegate.m
@@ -15,21 +15,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    CGRect viewRect = [[UIScreen mainScreen] bounds];
-    self.window = [[UIWindow alloc] initWithFrame:viewRect];
-    UIViewController *viewController = [[UIViewController alloc] init];
-    UIView *view = [[UIView alloc] initWithFrame:viewRect];
-
-    view.backgroundColor = [UIColor greenColor];
-
-    viewController.view = view;
-
-    self.window.rootViewController = viewController;
-    [self.window makeKeyAndVisible];
-
-    NSLog(@"Screen is %f tall and %f wide", viewRect.size.height, viewRect.size.width);
-
-    return YES;
+       return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
