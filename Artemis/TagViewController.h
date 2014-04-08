7f2e6ecd735bacbc1907b8703d05368ca897ad75
Liam Kilroy <Liam@Danas-MacBook-Pro.local>
2014-04-08 15:22:47 -0700

    Added tag screen code for naming tag

---
+++ Artemis/TagViewController.h
@@ -0,0 +1,13 @@
+//
+//  TagViewController.h
+//  Artemis
+//
+//  Created by Liam Kilroy on 4/2/14.
+//  Copyright (c) 2014 artemis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TagViewController : UITableViewController
+
+@end
