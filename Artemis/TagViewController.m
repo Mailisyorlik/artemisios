29b9e98ad271b7943c2ac023c0da7ccfa907a7b7
Liam Kilroy <Liam@Danas-MacBook-Pro.local>
2014-04-15 21:57:32 -0700

    Forgot to commit this. I think I just added more code for the table. Not sure

--- Artemis/TagViewController.m
+++ Artemis/TagViewController.m
@@ -30,7 +30,7 @@
     self.tags = [NSMutableArray arrayWithObjects:@"Wallet", @"Keys", nil];
     
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
-    
+        
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
