68f205912cd2b9c96c041dc61ecd1400c0e3259d
Liam <liamkilroy@gmail.com>
2014-12-02 08:39:02 -0800

    Added more basic stuff for hubs

---
+++ HubViewController.h
@@ -0,0 +1,20 @@
+//
+//  HubViewController.h
+//  Artemis
+//
+//  Created by Liam Kilroy on 12/1/14.
+//  Copyright (c) 2014 artemis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+#import "BLEDevice.h"
+#import "BLEUtility.h"
+
+
+
+@interface HubViewController : UIViewController
+
+
+
+@end
---
+++ HubViewController.m
@@ -0,0 +1,40 @@
+//
+//  HubViewController.m
+//  Artemis
+//
+//  Created by Liam Kilroy on 12/1/14.
+//  Copyright (c) 2014 artemis. All rights reserved.
+//
+
+#import "HubViewController.h"
+
+@interface HubViewController ()
+
+@end
+
+@implementation HubViewController
+
+//this code will not really be relevant until we have proof of concept for the hubs
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
